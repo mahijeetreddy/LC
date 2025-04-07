de807ec2d4aaea933832fb9799d1b221f4f31e08
Mahijeet Reddy Gaddam <92094066+mahijeetreddy@users.noreply.github.com>
2025-04-07 00:13:48 -0700

    Time: 11 ms (29.36%) | Memory: 19.2 MB (87.42%) - LeetSync

--- 169-majority-element/majority-element.py
+++ 169-majority-element/majority-element.py
@@ -10,24 +10,12 @@
 #             if count > s:
 #                 return nums[i]
 
-# class Solution:
-#     def majorityElement(self, nums: List[int]) -> int:
-#         s = len(nums) // 2
-#         count_map = {}
-
-#         for num in nums:
-#             count_map[num] = count_map.get(num, 0) + 1
-#             if count_map[num] > s:
-#                 return num
-
 class Solution:
     def majorityElement(self, nums: List[int]) -> int:
-        count = 0
-        candidate = None
+        s = len(nums) // 2
+        count_map = {}
 
         for num in nums:
-            if count == 0:
-                candidate = num
-            count += (1 if num == candidate else -1)
-
-        return candidate
+            count_map[num] = count_map.get(num, 0) + 1
+            if count_map[num] > s:
+                return num
