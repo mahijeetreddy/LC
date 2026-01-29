8b8102ab3b9fa1cfd39a2dc29a7586ae0e0e36ed
Mahijeet Reddy Gaddam <92094066+mahijeetreddy@users.noreply.github.com>
2026-01-28 19:29:43 -0800

    Time: 177 ms (63.86%) | Memory: 40.8 MB (22.91%) - LeetSync

---
+++ 148-sort-list/sort-list.py
@@ -0,0 +1,34 @@
+class Solution:
+    def sortList(self, head: Optional[ListNode]) -> Optional[ListNode]:
+        if not head or not head.next:
+            return head
+        
+        mid = self.getMid(head)
+        right = mid.next
+        mid.next = None
+
+        left = self.sortList(head)
+        right = self.sortList(right)
+
+        return self.merge(left, right)
+
+    def getMid(self, head):
+        slow, fast = head, head.next
+        while fast and fast.next:
+            slow = slow.next
+            fast = fast.next.next
+        return slow
+
+    def merge(self, list1, list2):
+        dummy = tail = ListNode()
+        while list1 and list2:
+            if list1.val < list2.val:
+                tail.next = list1
+                list1 = list1.next
+            else:
+                tail.next = list2
+                list2 = list2.next
+            tail = tail.next
+
+        tail.next = list1 or list2
+        return dummy.next
