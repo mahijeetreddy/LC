b599a79a0aa8a18d258c0a693bb5cf9fc95cd338
Mahijeet Reddy Gaddam <92094066+mahijeetreddy@users.noreply.github.com>
2026-06-29 23:16:55 -0700

    Time: 0 ms (100.00%) | Memory: 19.4 MB (86.66%) - LeetSync

--- 46-permutations/permutations.py
+++ 46-permutations/permutations.py
@@ -6,9 +6,8 @@ class Solution:
         perms = self.permute(nums[1:])
         res = []
         for p in perms:
-            for i in range(len(p) + 1):
+            for i in range(len(p) +1):
                 p_copy = p.copy()
                 p_copy.insert(i, nums[0])
                 res.append(p_copy)
-            
         return res
