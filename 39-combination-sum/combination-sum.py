ee9a5ffc4ad75bf3faffaa04ff45916d611b6291
Mahijeet Reddy Gaddam <92094066+mahijeetreddy@users.noreply.github.com>
2026-06-29 20:53:22 -0700

    Time: 9 ms (54.26%) | Memory: 19.6 MB (31.79%) - LeetSync

--- 39-combination-sum/combination-sum.py
+++ 39-combination-sum/combination-sum.py
@@ -1,6 +1,7 @@
 class Solution:
     def combinationSum(self, candidates: List[int], target: int) -> List[List[int]]:
         res = []
+
         def dfs(i, cur, total):
             if total == target:
                 res.append(cur.copy())
@@ -11,5 +12,5 @@ class Solution:
             dfs(i, cur, total + candidates[i])
             cur.pop()
             dfs(i+1, cur, total)
-        dfs(0,[],0)
+        dfs(0, [], 0)
         return res
