163fad2392c353d66be98cf29bfed618ac8541e6
Mahijeet Reddy Gaddam <92094066+mahijeetreddy@users.noreply.github.com>
2026-08-06 14:37:09 -0700

    Time: 131 ms (46.09%) | Memory: 19.2 MB (67.38%) - LeetSync

--- 647-palindromic-substrings/palindromic-substrings.py
+++ 647-palindromic-substrings/palindromic-substrings.py
@@ -1,13 +1,27 @@
 class Solution:
     def countSubstrings(self, s: str) -> int:
-        def checkpal(subs):
-            if subs == subs[::-1]:
-                return True
+        # def checkpal(subs):
+        #     if subs == subs[::-1]:
+        #         return True
+        # result = 0
+
+        # for i in range(len(s)):
+        #     for j in range(i,len(s)):
+        #         substringx = s[i:j+1]
+        #         if checkpal(substringx):
+        #             result+=1
+        # return result
         result = 0
 
+        def expand(left, right):
+            count = 0
+            while left>=0 and right< len(s) and s[left] == s[right]:
+                count+=1
+                left -=1
+                right +=1
+            return count
         for i in range(len(s)):
-            for j in range(i,len(s)):
-                substringx = s[i:j+1]
-                if checkpal(substringx):
-                    result+=1
+            result += expand(i,i)
+            result+= expand(i, i+1)
+
         return result
