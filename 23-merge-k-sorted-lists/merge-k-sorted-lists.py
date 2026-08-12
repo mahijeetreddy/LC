35dedc4704e85138d54db48bfd5063bb61a66d0f
Mahijeet Reddy Gaddam <92094066+mahijeetreddy@users.noreply.github.com>
2026-08-12 12:14:08 -0700

    Time: 953 ms (9.87%) | Memory: 22.6 MB (92.97%) - LeetSync

--- 23-merge-k-sorted-lists/merge-k-sorted-lists.py
+++ 23-merge-k-sorted-lists/merge-k-sorted-lists.py
@@ -5,32 +5,24 @@
 #         self.next = next
 class Solution:
     def mergeKLists(self, lists: List[Optional[ListNode]]) -> Optional[ListNode]:
-        if not lists or len(lists) == 0:
+        if not lists:
             return None
-        
-        while len(lists) > 1:
-            mergedLists = []
-            for i in range(0, len(lists), 2):
-                l1 = lists[i]
-                l2 = lists[i+1] if (i+1) < len(lists ) else None
-                mergedLists.append(self.mergeLists(l1, l2))
-            lists = mergedLists
-        return lists[0]
-    
-    def mergeLists(self, l1, l2):
-        dummy = ListNode()
-        tail = dummy
-        while l1 and l2:
-            if l1.val < l2.val:
-                tail.next = l1
-                l1= l1.next
-            else:
-                tail.next = l2
-                l2 = l2.next
-            tail = tail.next
-        if l1:
-            tail.next = l1
-        if l2:
-            tail.next = l2
+        def merge2(list1, list2):
+            dummy = ListNode(0)
+            tail = dummy
+            curr1 , curr2 = list1, list2
+            while curr1 and curr2:
+                if curr1.val<= curr2.val:
+                    tail.next = curr1
+                    curr1 = curr1.next
+                else:
+                    tail.next = curr2
+                    curr2 = curr2.next
+                tail = tail.next
+            tail.next = curr1 or curr2
+            return dummy.next
 
-        return dummy.next
+        for i in range(len(lists) -1):
+            lists[i+1] = merge2(lists[i], lists[i+1])
+
+        return lists[-1]
