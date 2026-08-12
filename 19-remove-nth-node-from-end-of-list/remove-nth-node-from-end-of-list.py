1f1b587d0413d2c616e24964f60e6554e4e5d8d6
Mahijeet Reddy Gaddam <92094066+mahijeetreddy@users.noreply.github.com>
2026-08-12 09:06:00 -0700

    Time: 0 ms (100.00%) | Memory: 19.1 MB (91.21%) - LeetSync

--- 19-remove-nth-node-from-end-of-list/remove-nth-node-from-end-of-list.py
+++ 19-remove-nth-node-from-end-of-list/remove-nth-node-from-end-of-list.py
@@ -5,29 +5,46 @@
 #         self.next = next
 class Solution:
     def removeNthFromEnd(self, head: Optional[ListNode], n: int) -> Optional[ListNode]:
-        def helper(head):
-            if not head:
-                return
-            prev, curr = None, head
-            while curr:
-                nxt = curr.next
-                curr.next = prev
-                prev = curr
-                curr = nxt
-            return prev
-        
-        mainprev = helper(head) #got the reverse:
         
         dummy = ListNode(0)
-        dummy.next = mainprev
+        dummy.next = head
 
-        prev = dummy
-        curr = mainprev
+        slow, fast = dummy, dummy
 
-        for _ in range(n-1):
-            prev = curr
-            curr = curr.next
+        for _ in range(n+1):
+            fast = fast.next
         
-        prev.next = curr.next
+        while fast:
+            slow = slow.next
+            fast = fast.next
+        slow.next = slow.next.next
+        return dummy.next
 
-        return helper(dummy.next)
+
+        
+        # def helper(head):
+        #     if not head:
+        #         return
+        #     prev, curr = None, head
+        #     while curr:
+        #         nxt = curr.next
+        #         curr.next = prev
+        #         prev = curr
+        #         curr = nxt
+        #     return prev
+        
+        # mainprev = helper(head) #got the reverse:
+        
+        # dummy = ListNode(0)
+        # dummy.next = mainprev
+
+        # prev = dummy
+        # curr = mainprev
+
+        # for _ in range(n-1):
+        #     prev = curr
+        #     curr = curr.next
+        
+        # prev.next = curr.next
+
+        # return helper(dummy.next)
