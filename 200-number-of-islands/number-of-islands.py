16474c1bb4c2abf20483b3b998c056369444bf57
Mahijeet Reddy Gaddam <92094066+mahijeetreddy@users.noreply.github.com>
2026-08-12 14:29:44 -0700

    Time: 290 ms (8.35%) | Memory: 31.4 MB (6.90%) - LeetSync

--- 200-number-of-islands/number-of-islands.py
+++ 200-number-of-islands/number-of-islands.py
@@ -8,11 +8,12 @@ class Solution:
             if r<0 or c<0 or r==ROWS or c == COLS or (r,c) in visit or grid[r][c] == "0":
                 return 0
             directions = [[0,1],[0,-1],[1,0],[-1,0]]
+            visit.add((r,c))
             for dr, dc in directions:
                 row = dr+r
                 col = dc + c
                 dfs(row,col)
-                visit.add((r,c))
+            
 
 
         for r in range(ROWS):
