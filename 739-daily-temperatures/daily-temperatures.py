cf26bd9db02bcb6c821748c1953a91117b5cff0d
Mahijeet Reddy Gaddam <92094066+mahijeetreddy@users.noreply.github.com>
2026-08-13 14:06:02 -0700

    Time: 130 ms (13.09%) | Memory: 36.1 MB (5.44%) - LeetSync

--- 739-daily-temperatures/daily-temperatures.py
+++ 739-daily-temperatures/daily-temperatures.py
@@ -6,6 +6,6 @@ class Solution:
         for i,t in enumerate(temperatures):
             while stack and t> stack[-1][0]:
                 stackT, stackInd = stack.pop()
-                res[stackInd] = (i - stackInd)
+                res[stackInd] = i - stackInd
             stack.append([t,i])
         return res
