8b3410218422af4b79946f71d953ed4008a5b088
Mahijeet Reddy Gaddam <92094066+mahijeetreddy@users.noreply.github.com>
2026-08-13 17:26:20 -0700

    Time: 3 ms (41.46%) | Memory: 22.5 MB (32.95%) - LeetSync

--- 104-maximum-depth-of-binary-tree/maximum-depth-of-binary-tree.py
+++ 104-maximum-depth-of-binary-tree/maximum-depth-of-binary-tree.py
@@ -9,8 +9,8 @@ class Solution:
         def dfs(node):
             if not node:
                 return 0
-            
-            left_depth = dfs(node.left)
-            right_depth = dfs(node.right)
-            return 1 + max(left_depth, right_depth)
+
+            ld = dfs(node.left)
+            rd = dfs(node.right)
+            return 1 + max(ld, rd)
         return dfs(root)
