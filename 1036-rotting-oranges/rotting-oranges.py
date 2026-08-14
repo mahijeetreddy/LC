f7ecb5a902514673a4e495da72fa6f4b0ec8f03c
Mahijeet Reddy Gaddam <92094066+mahijeetreddy@users.noreply.github.com>
2026-08-13 19:39:31 -0700

    Time: 3 ms (74.78%) | Memory: 19.2 MB (96.24%) - LeetSync

--- 1036-rotting-oranges/rotting-oranges.py
+++ 1036-rotting-oranges/rotting-oranges.py
@@ -1,26 +1,46 @@
+from collections import deque
 class Solution:
     def orangesRotting(self, grid: List[List[int]]) -> int:
-        ROWS , COLS = len(grid), len(grid[0])
-        fresh , time = 0,0
-        q = deque()
+        ROWS, COLS = len(grid), len(grid[0])
+        mins = 0
+
+        queue = deque()
+        fresh = 0
 
         for r in range(ROWS):
             for c in range(COLS):
-                if grid[r][c] == 1:
-                    fresh +=1
                 if grid[r][c] == 2:
-                    q.append([r,c])
-        
-        directions = [[0,1],[0,-1],[1,0],[-1,0]]
-        while q and fresh >0:
-            for i in range(len(q)):
-                r,c = q.popleft()
+                    queue.append((r,c))
+                elif grid[r][c] == 1:
+                    fresh +=1
+
+        if fresh == 0:
+            return 0
+
+        directions = [[0,1],[1,0],[-1,0],[0,-1]]
+
+        while queue and fresh>0:
+            level = len(queue)
+
+            for _ in range(level):
+                r,c = queue.popleft()
+
                 for dr, dc in directions:
-                    row, col = dr + r, dc + c
-                    if (row<0 or col<0 or row == ROWS or col == COLS or grid[row][col] != 1):
-                        continue
-                    grid[row][col] = 2
-                    q.append([row, col])
-                    fresh -=1
-            time +=1
-        return time if fresh ==0 else -1
+                    row = r+ dr
+                    col = c + dc
+
+                    if 0<= row< ROWS and 0 <= col < COLS:
+                        if grid[row][col] == 1:
+                            grid[row][col] = 2
+                            fresh -=1
+                            queue.append((row, col))
+                    
+            mins +=1
+
+        if fresh >0:
+            return -1
+        
+        return mins
+
+
+
