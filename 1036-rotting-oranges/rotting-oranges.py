98a76d20a73b883abcfab56c22023c0738a2b17f
Mahijeet Reddy Gaddam <92094066+mahijeetreddy@users.noreply.github.com>
2026-08-15 19:02:10 -0700

    Time: 3 ms (74.65%) | Memory: 19.3 MB (78.77%) - LeetSync

--- 1036-rotting-oranges/rotting-oranges.py
+++ 1036-rotting-oranges/rotting-oranges.py
@@ -1,29 +1,31 @@
 from collections import deque
 class Solution:
     def orangesRotting(self, grid: List[List[int]]) -> int:
-        fresh = 0
         mins = 0
+        fresh = 0
         q = deque()
 
         ROWS, COLS = len(grid), len(grid[0])
 
-        for i in range(ROWS):
-            for j in range(COLS):
-                if grid[i][j] == 1:
+        for r in range(ROWS):
+            for c in range(COLS):
+                if grid[r][c] == 1:
                     fresh +=1
-                elif grid[i][j] == 2:
-                    q.append((i,j))
-        directions = [(1,0),(-1,0),(0,1),(0,-1)]
-        while q and fresh>0:
+                elif grid[r][c] == 2:
+                    q.append((r,c))
+
+        directions = [(0,1),(0,-1),(1,0),(-1,0)]
+
+        while q and fresh >0:
             for _ in range(len(q)):
-                r,c = q.popleft()
+                r, c = q.popleft()
                 for dr, dc in directions:
                     row = r + dr
                     col = c + dc
 
-                    if (0<= row< ROWS and 0 <= col < COLS and grid[row][col] == 1):
+                    if (0<=row< ROWS and 0<= col < COLS and grid[row][col] == 1):
                         grid[row][col] = 2
+                        q.append((row, col))
                         fresh -=1
-                        q.append((row,col))
             mins +=1
         return mins if fresh == 0 else -1
