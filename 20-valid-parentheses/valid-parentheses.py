d1e6ceba8c58719db3874abbbc71f81ff5c5524c
Mahijeet Reddy Gaddam <92094066+mahijeetreddy@users.noreply.github.com>
2026-08-15 17:34:53 -0700

    Time: 3 ms (31.92%) | Memory: 19.2 MB (91.01%) - LeetSync

--- 20-valid-parentheses/valid-parentheses.py
+++ 20-valid-parentheses/valid-parentheses.py
@@ -5,8 +5,8 @@ class Solution:
             "]":"[",
             ")":"("
         }
-        stack = []
 
+        stack = []
         for c in s:
             if c in closeToOpen:
                 if stack and stack[-1] == closeToOpen[c]:
