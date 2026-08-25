e4b0f911f134f7d1b1a0fe97021a6342040afa4d
Mahijeet Reddy Gaddam <92094066+mahijeetreddy@users.noreply.github.com>
2026-08-25 12:56:19 -0700

    Time: 88 ms (36.31%) | Memory: 27.8 MB (60.87%) - LeetSync

--- 931-maximum-frequency-stack/maximum-frequency-stack.py
+++ 931-maximum-frequency-stack/maximum-frequency-stack.py
@@ -1,18 +1,14 @@
-from collections import Counter
 class FreqStack:
-
     def __init__(self):
-        self.stacks = {}
+        self.stacks = defaultdict(list)
         self.cnt = Counter()
         self.maxCnt = 0
 
     def push(self, val: int) -> None:
         self.cnt[val] +=1
-        valCnt = self.cnt[val]
-        if valCnt> self.maxCnt:
-            self.maxCnt = valCnt
-            self.stacks[valCnt] = []
-        self.stacks[valCnt].append(val)
+        freq = self.cnt[val]
+        self.maxCnt = max(self.maxCnt, freq)
+        self.stacks[freq].append(val)
 
     def pop(self) -> int:
         res = self.stacks[self.maxCnt].pop()
