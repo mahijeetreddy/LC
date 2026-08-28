c3a836f9e12921b2bf01faaf4292e219f9b3daa1
Mahijeet Reddy Gaddam <92094066+mahijeetreddy@users.noreply.github.com>
2026-08-27 20:31:08 -0700

    Time: 7 ms (88.62%) | Memory: 22.5 MB (84.99%) - LeetSync

--- 1039-find-the-town-judge/find-the-town-judge.py
+++ 1039-find-the-town-judge/find-the-town-judge.py
@@ -3,10 +3,9 @@ class Solution:
         score = [0] * (n+1)
 
         for a,b in trust:
-            score[a] -= 1
-            score[b] += 1
-
+            score[a] -=1
+            score[b] +=1
         for person in range(1, n+1):
-            if score[person] == n - 1:
+            if score[person] == n-1:
                 return person
         return -1
