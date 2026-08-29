f8f86e4d9bf0a54b7c288fa3a923a36fa700a183
Mahijeet Reddy Gaddam <92094066+mahijeetreddy@users.noreply.github.com>
2026-08-29 13:55:06 -0700

    Time: 4 ms (81.03%) | Memory: 23.8 MB (10.70%) - LeetSync

--- 229-majority-element-ii/majority-element-ii.py
+++ 229-majority-element-ii/majority-element-ii.py
@@ -2,11 +2,10 @@ class Solution:
     def majorityElement(self, nums: List[int]) -> List[int]:
         n = len(nums)
         threshold = n//3
-        output = []
-        count = Counter()
-        for n in nums:
-            count[n] +=1
-        for n,c in count.items():
-            if c> threshold:
-                output.append(n)
-        return output
+        res = []
+        mapp = Counter(nums)
+
+        for t,c in mapp.items():
+            if c > threshold:
+                res.append(t)
+        return res
