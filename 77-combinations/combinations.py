eb358a9c49d05872a2fc33a6cc3416b9e7a3d199
Mahijeet Reddy Gaddam <92094066+mahijeetreddy@users.noreply.github.com>
2026-08-31 10:09:00 -0700

    Time: 107 ms (66.37%) | Memory: 61.4 MB (54.09%) - LeetSync

--- 77-combinations/combinations.py
+++ 77-combinations/combinations.py
@@ -1,15 +1,14 @@
 class Solution:
     def combine(self, n: int, k: int) -> List[List[int]]:
         res = []
-        comb = []
 
-        def dfs(i):
+        def backtrack(start, comb):
             if len(comb) == k:
                 res.append(comb.copy())
                 return
-            for j in range(i, n+1):
-                comb.append(j)
-                dfs(j+1)
+            for i in range(start, n+1):
+                comb.append(i)
+                backtrack(i+1, comb)
                 comb.pop()
-        dfs(1)
+        backtrack(1,[])
         return res
