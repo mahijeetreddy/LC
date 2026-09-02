6b61913db782b76816548c0eadf6ebc22f9f833f
Mahijeet Reddy Gaddam <92094066+mahijeetreddy@users.noreply.github.com>
2026-09-02 14:05:28 -0700

    Time: 20 ms (61.49%) | Memory: 25.5 MB (69.85%) - LeetSync

--- 238-product-of-array-except-self/product-of-array-except-self.py
+++ 238-product-of-array-except-self/product-of-array-except-self.py
@@ -6,7 +6,7 @@ class Solution:
         for i in range(len(nums)):
             ans[i] = pre
             pre *= nums[i]
-        for i in range(len(nums) - 1,-1,-1):
-            ans[i]*= post
-            post *= nums[i]
+        for i in range(len(nums) - 1, -1, -1):
+            ans[i] *= post
+            post*= nums[i]
         return ans
