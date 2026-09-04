a9d5646d7f628408b720f66ccb5abb0861bd1d7c
Mahijeet Reddy Gaddam <92094066+mahijeetreddy@users.noreply.github.com>
2026-09-03 18:37:32 -0700

    Time: 48 ms (18.24%) | Memory: 20.5 MB (13.83%) - LeetSync

--- 572-subtree-of-another-tree/subtree-of-another-tree.py
+++ 572-subtree-of-another-tree/subtree-of-another-tree.py
@@ -4,17 +4,22 @@
 #         self.val = val
 #         self.left = left
 #         self.right = right
-class Solution:
-    def isSubtree(self, root: Optional[TreeNode], subRoot: Optional[TreeNode]) -> bool:
-        if not subRoot: return True
-        if not root: return False
-        if self.sameTree(root,subRoot):
-            return True
-        return (self.isSubtree(root.left,subRoot) or self.isSubtree(root.right, subRoot))
 
-    def sameTree(self, s,t):
-        if not s and not t:
+class Solution:   
+    def isSubtree(self, root: Optional[TreeNode], subRoot: Optional[TreeNode]) -> bool:
+        def same(a,b):
+            if not a and not b:
+                return True
+            if not a or not b:
+                return False
+            if a.val != b.val:
+                return False
+            return same(a.left, b.left) and same(a.right, b.right)
+        
+        if not subRoot:
             return True
-        if s and t and s.val == t.val:
-            return (self.sameTree(s.left, t.left) and self.sameTree(s.right, t.right))
-        return False
+        if not root:
+            return False
+        if same(root, subRoot):
+            return True
+        return self.isSubtree(root.left, subRoot) or self.isSubtree(root.right, subRoot)
