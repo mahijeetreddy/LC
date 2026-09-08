847a4bc5c722fe7e48919fbd58503d7c42b4dc7e
Mahijeet Reddy Gaddam <92094066+mahijeetreddy@users.noreply.github.com>
2026-09-08 13:29:35 -0700

    Time: 3 ms (85.88%) | Memory: 21 MB (37.92%) - LeetSync

--- 207-course-schedule/course-schedule.py
+++ 207-course-schedule/course-schedule.py
@@ -1,6 +1,6 @@
 class Solution:
     def canFinish(self, numCourses: int, prerequisites: List[List[int]]) -> bool:
-        preMap = {i:[] for i in range(numCourses)}
+        preMap = defaultdict(list)
 
         for crs, pre in prerequisites:
             preMap[crs].append(pre)
