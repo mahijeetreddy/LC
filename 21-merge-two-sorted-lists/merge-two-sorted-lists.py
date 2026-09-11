c1255204d62f1dffffb7b81a4b3e8f202a87ae75
Mahijeet Reddy Gaddam <92094066+mahijeetreddy@users.noreply.github.com>
2026-09-11 15:21:25 -0700

    Time: 0 ms (100.00%) | Memory: 19.3 MB (73.53%) - LeetSync

--- 21-merge-two-sorted-lists/merge-two-sorted-lists.py
+++ 21-merge-two-sorted-lists/merge-two-sorted-lists.py
@@ -8,6 +8,7 @@ class Solution:
         dummy = ListNode(0)
         tail = dummy
         curr1, curr2 = list1, list2
+
         while curr1 and curr2:
             if curr1.val <= curr2.val:
                 tail.next = curr1
@@ -18,4 +19,3 @@ class Solution:
             tail = tail.next
         tail.next = curr1 or curr2
         return dummy.next
-        
