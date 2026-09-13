1c4e3dbb33db834e5585624a26b92fabe46fc836
Mahijeet Reddy Gaddam <92094066+mahijeetreddy@users.noreply.github.com>
2026-09-13 14:09:23 -0700

    Time: 7 ms (44.25%) | Memory: 21.2 MB (25.37%) - LeetSync

--- 207-course-schedule/course-schedule.py
+++ 207-course-schedule/course-schedule.py
@@ -1,16 +1,15 @@
+
 class Solution:
     def canFinish(self, numCourses: int, prerequisites: List[List[int]]) -> bool:
         preMap = defaultdict(list)
-
+        visit = set()
         for crs, pre in prerequisites:
             preMap[crs].append(pre)
-        visit = set()
-
+        
         def dfs(crs):
-            if crs in visit:
-                return False
-            if preMap[crs] == []:
-                return True
+            if crs in visit: return False
+            
+            if preMap[crs] == []: return True
             visit.add(crs)
             for pre in preMap[crs]:
                 if not dfs(pre): return False
