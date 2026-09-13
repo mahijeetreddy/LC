731d98aeeb45edcda4833ef6a00553060e1be4a6
Mahijeet Reddy Gaddam <92094066+mahijeetreddy@users.noreply.github.com>
2026-09-13 11:19:32 -0700

    Time: 91 ms (43.28%) | Memory: 22.5 MB (62.77%) - LeetSync

--- 297-serialize-and-deserialize-binary-tree/serialize-and-deserialize-binary-tree.py
+++ 297-serialize-and-deserialize-binary-tree/serialize-and-deserialize-binary-tree.py
@@ -9,6 +9,7 @@ class Codec:
 
     def serialize(self, root):
         res = []
+
         def dfs(node):
             if not node:
                 res.append("N")
@@ -16,19 +17,22 @@ class Codec:
             res.append(str(node.val))
             dfs(node.left)
             dfs(node.right)
+
         dfs(root)
         return ",".join(res)
         
 
     def deserialize(self, data):
         vals = data.split(",")
-        self.i = 0
+        i = 0
         def dfs():
-            if vals[self.i] == "N":
-                self.i+=1
+            nonlocal i
+            if vals[i] == "N":
+                i+=1
                 return None
-            node = TreeNode(int(vals[self.i]))
-            self.i+=1
+            
+            node = TreeNode(int(vals[i]))
+            i+=1
             node.left = dfs()
             node.right = dfs()
             return node
