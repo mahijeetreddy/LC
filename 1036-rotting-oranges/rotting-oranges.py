5bc6cbd6d35096ee5b858ec45b9a7c712cdb63b0
Mahijeet Reddy Gaddam <92094066+mahijeetreddy@users.noreply.github.com>
2026-09-16 13:03:32 -0700

    Time: 3 ms (75.69%) | Memory: 19.3 MB (42.87%) - LeetSync

--- 1036-rotting-oranges/rotting-oranges.py
+++ 1036-rotting-oranges/rotting-oranges.py
@@ -1,26 +1,31 @@
 class Solution:
-    def orangesRotting(self, grid: List[List[int]]) -> int:
+    def orangesRotting(self, grid: list[list[int]]) -> int:
+        ROWS, COLS = len(grid), len(grid[0])
         fresh = 0
         minutes = 0
         q = deque()
-        ROWS, COLS = len(grid), len(grid[0])
+
         for r in range(ROWS):
             for c in range(COLS):
                 if grid[r][c] == 1:
-                    fresh+=1
+                    fresh +=1
+
                 elif grid[r][c] == 2:
                     q.append((r,c))
         directions = [[0,1],[1,0],[-1,0],[0,-1]]
-        while q and fresh>0:
+        
+        while q and fresh >0:
             for _ in range(len(q)):
+                
                 r,c = q.popleft()
                 for dr, dc in directions:
-                    row = dr + r
-                    col = dc + c
-                    if (0<=row< ROWS  and 0<= col<COLS and grid[row][col] == 1):
-                        grid[row][col] = 2
-                        q.append((row, col))
-                        fresh -=1
-            minutes +=1
-        return minutes if fresh == 0 else -1
+                    row, col = r + dr, c + dc
 
+                    if 0<= row < ROWS and 0<= col < COLS:
+                        if grid[row][col] == 1:
+                            grid[row][col] = 2
+                            fresh -=1
+                            q.append((row, col))
+            minutes +=1
+
+        return minutes if fresh == 0 else -1
