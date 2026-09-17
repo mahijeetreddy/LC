78dc9c184270812f5a2e93416aee30f10837c52a
Mahijeet Reddy Gaddam <92094066+mahijeetreddy@users.noreply.github.com>
2026-09-17 09:33:32 -0700

    Time: 0 ms (100.00%) | Memory: 20.5 MB (42.06%) - LeetSync

--- 1-two-sum/two-sum.py
+++ 1-two-sum/two-sum.py
@@ -1,7 +1,7 @@
 class Solution:
     def twoSum(self, nums: list[int], target: int) -> list[int]:
         hmap = {}
-        for i, a in enumerate(nums):
+        for i,a in enumerate(nums):
             complement = target - a
             if complement in hmap:
                 return [hmap[complement], i]
