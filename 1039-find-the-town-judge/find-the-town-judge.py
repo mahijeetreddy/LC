9b5f8cdb6a348e6a239908d0b2cc05afb12c89cc
Mahijeet Reddy Gaddam <92094066+mahijeetreddy@users.noreply.github.com>
2026-09-17 10:16:45 -0700

    Time: 23 ms (23.66%) | Memory: 22.6 MB (36.72%) - LeetSync

--- 1039-find-the-town-judge/find-the-town-judge.py
+++ 1039-find-the-town-judge/find-the-town-judge.py
@@ -3,10 +3,9 @@ class Solution:
         incoming, outgoing = defaultdict(int), defaultdict(int)
 
         for src, dst in trust:
-            outgoing[src] = outgoing[src] + 1
             incoming[dst] +=1
-        
+            outgoing[src] +=1
         for i in range(1, n+1):
-            if outgoing[i] == 0 and incoming[i] == n - 1:
+            if outgoing[i] == 0 and incoming[i] == n -1 :
                 return i
         return -1
