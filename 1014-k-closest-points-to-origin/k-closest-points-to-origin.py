4f131f40c4ce1f4847b313139ab6011721e2bdfb
Mahijeet Reddy Gaddam <92094066+mahijeetreddy@users.noreply.github.com>
2026-09-17 10:12:42 -0700

    Time: 86 ms (17.00%) | Memory: 24.5 MB (78.00%) - LeetSync

--- 1014-k-closest-points-to-origin/k-closest-points-to-origin.py
+++ 1014-k-closest-points-to-origin/k-closest-points-to-origin.py
@@ -3,9 +3,7 @@ class Solution:
         maxHeap = []
         for point in points:
             dist = point[0]**2 + point[1]**2
-            heapq.heappush(maxHeap, (-dist, point[0], point[1]))
-
+            heapq.heappush(maxHeap, [-dist, point[0], point[1]])
             if len(maxHeap) > k:
                 heapq.heappop(maxHeap)
-        
         return [[point[1], point[2]] for point in maxHeap]
